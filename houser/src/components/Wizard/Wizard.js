20551200a801f8f72c1a65ef966f98341fe1dbdc
breakingBrad <bradvanorman@gmail.com>
2018-07-07 10:18:58 -0600

    part 1, step 2 complete

--- houser/src/components/Wizard/Wizard.js
+++ houser/src/components/Wizard/Wizard.js
@@ -2,16 +2,46 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class Wizard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      address: '',
+      city: '',
+      state: '',
+      zipcode: '',
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+
+  }
   render() {
     return (
       <div>
         Wizard
       <br />
         <Link to="/">
-          <button type="submit">
-            Cancel
-          </button>
+          <button type="submit">Cancel</button>
         </Link>
+        <br />
+        <b>Property Name</b>
+        <input className="input-line" type="text" name="name" onChange={this.handleChange} />
+        <br />
+        <b>Address</b>
+        <input className="input-line" type="text" name="address" onChange={this.handleChange} />
+        <br />
+        <b>City</b>
+        <input className="input-line" type="text" name="city" onChange={this.handleChange} />
+        <b>State</b>
+        <input
+          className="input-line" type="text" name="state" onChange={this.handleChange} maxLength="2" />
+        <br />
+        <b>Zip</b>
+        <input className="input-line" type="number" name="zip" onChange={this.handleChange} placeholder="0" />
+        <br />
       </div>
     )
   }
